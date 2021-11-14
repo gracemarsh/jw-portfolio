f775b319e79db6efac888d24018ffe91f0197703
Grace <gracemarsh27@gmail.com>
2021-11-15 10:36:19 +1100

    update css nabvar look

--- main.js
+++ main.js
@@ -3,19 +3,27 @@
 //   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 // `;
 
-var exampleModal = document.getElementById("exampleModal");
-exampleModal.addEventListener("show.bs.modal", function (event) {
-  // Button that triggered the modal
-  var button = event.relatedTarget;
-  // Extract info from data-bs-* attributes
-  var recipient = button.getAttribute("data-bs-whatever");
-  // If necessary, you could initiate an AJAX request here
-  // and then do the updating in a callback.
-  //
-  // Update the modal's content.
-  var modalTitle = exampleModal.querySelector(".modal-title");
-  var modalBodyInput = exampleModal.querySelector(".modal-body input");
+// ============ BACK TO TOP BUTTON ============
 
-  modalTitle.textContent = "New message to " + recipient;
-  modalBodyInput.value = recipient;
-});
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
