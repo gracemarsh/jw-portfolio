34db2f21e8c9c707cb1ef75a8c5abbaa250918dd
Grace <gracemarsh27@gmail.com>
2022-01-04 23:50:05 +1100

    update css

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 // ============ BACK TO TOP BUTTON ============
 
 //Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+let scrollToTopButton = document.getElementById("btn-back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
@@ -15,13 +15,13 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    scrollToTopButton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    scrollToTopButton.style.display = "none";
   }
 }
 // When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
+scrollToTopButton.addEventListener("click", backToTop);
 
 function backToTop() {
   document.body.scrollTop = 0;
